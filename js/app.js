5a0dd841e51178ee0760ba10c71faca9523f7dba
Mateo Ruibal <mateoruibal06@gmail.com>
2023-05-06 10:51:48 -0300

    Portfolio finished

--- js/app.js
+++ js/app.js
@@ -12,18 +12,3 @@ menu.onclick = () => {
 window.onscroll = () => {
   navbar.classList.remove("active");
 };
-
-// Dark Mode / light mode
-let darkmode = document.querySelector("#darkmode");
-
-darkmode.onclick = () => {
-  if (darkmode.classList.contains("bx-moon")) {
-    darkmode.classList.replace("bx-moon", "bx-sun");
-    document.body.classList.add("active");
-  } else {
-    darkmode.classList.replace("bx-sun", "bx-moon");
-    document.body.classList.remove("active");
-  }
-};
-
-                                    
